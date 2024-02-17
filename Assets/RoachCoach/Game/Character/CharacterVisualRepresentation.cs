6f6883ddf5686c0bcac81304ee03eb946282c27c
Fahmy <fahmy.eldeeb@outlook.com>
2024-02-17 06:50:38 +0100

    Some quick visuals added with dirty code/ you don't need this push

--- Assets/RoachCoach/Game/Character/CharacterVisualRepresentation.cs
+++ Assets/RoachCoach/Game/Character/CharacterVisualRepresentation.cs
@@ -1,6 +1,7 @@
 
 using RoachCoach.Game;
 using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,18 @@ namespace RoachCoach
     {
         [SerializeField] GameObject canvas;
         [SerializeField] Image circleFill;
+        float speed;
+        Vector3 lastPos;
+        Animator animator;
+        bool init;
+        private async void Awake()
+        {
+            animator = GetComponent<Animator>();
+            animator.speed = 0;
+            await Task.Delay(Random.Range(500, 3000));
+            animator.speed = 1;
+            init= true;
+        }
         public override void Link(Entitas.Entity entity)
         {
             base.Link(entity);
@@ -19,12 +32,15 @@ namespace RoachCoach
         }
         public void OnDelayAdded(Entity entity, float valueInSeconds)
         {
+            if (canvas == null) return;
             StopAllCoroutines();
             StartCoroutine(DoOverTime(entity.GetDelay().ValueInSeconds));
         }
 
         public void OnDelayRemoved(Entity entity)
         {
+            if (canvas == null) return;
+
             canvas.SetActive(false);
             StopAllCoroutines();
         }
@@ -38,6 +54,16 @@ namespace RoachCoach
                 circleFill.fillAmount = 1 - (elapsed / duration);
                 yield return null;
             }
+        }
+        private void Update()
+        {
+            if (!init) return;
+            speed = (transform.position - lastPos).magnitude;
+            lastPos = transform.position;
+            if (speed < 0.01f)
+                animator.speed = 0.5f;
+            else
+                animator.speed = 2.5f;
         }
     }
 
